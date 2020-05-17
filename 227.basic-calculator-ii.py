0e87848b049c14476fe79a99d9e1faf809f14c6c
Akash Devdhar <neospeed83@gmail.com>
2020-05-16 18:38:08 -0700

    BST search

---
+++ 227.basic-calculator-ii.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=227 lang=python3
+#
+# [227] Basic Calculator II
+#
+
+# @lc code=start
+class Solution:
+    def calculate(self, s: str) -> int:
+        s = s.strip()
+        if '*' in s:
+            
+
+
+# @lc code=end
+
